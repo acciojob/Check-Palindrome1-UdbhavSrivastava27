7ac334cf5201f0537d159027285d742f4303de11
Udbhav Srivastava <106414192+UdbhavSrivastava27@users.noreply.github.com>
2023-05-21 04:07:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // complete the given function
 
 function palindrome(str){
-
+	let right;
+     for(let i = 0 ; i <str.length; i++){
+         right = arr[i];
+	 }
+	let left;
+	for(let i = str.length-1 ; i >=0 ; i--){
+		left = arr[i];
+	}
+	if(right == left){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 module.exports = palindrome
